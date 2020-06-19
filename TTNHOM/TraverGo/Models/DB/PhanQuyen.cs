e825b62b6c3a43307f422e65c5c74f94d5612076
PDA-Khacer <59501767_PDA_Khacer@users.noreply.github.com>
2020-06-19 10:34:35 +0700

    Design layout and add Model
    Tạo layout cơ bản và thêm model vào project

---
+++ TTNHOM/TraverGo/Models/DB/PhanQuyen.cs
@@ -0,0 +1,25 @@
+namespace TraverGo.Models.DB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("PhanQuyen")]
+    public partial class PhanQuyen
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public PhanQuyen()
+        {
+            Customers = new HashSet<Customer>();
+        }
+
+        [Key]
+        [StringLength(100)]
+        public string nameQ { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Customer> Customers { get; set; }
+    }
+}
---
+++ TTNHOM/TraverGo/Models/DB/TourDestination.cs
@@ -0,0 +1,76 @@
+namespace TraverGo.Models.DB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("TourDestination")]
+    public partial class TourDestination
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public TourDestination()
+        {
+            Blogs = new HashSet<Blog>();
+            DetailCarts = new HashSet<DetailCart>();
+            HomeStays = new HashSet<HomeStay>();
+            Taxis = new HashSet<Taxi>();
+            Trips = new HashSet<Trip>();
+            Trips1 = new HashSet<Trip>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        public string maDD { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string maTinh { get; set; }
+
+        [StringLength(100)]
+        public string tenDD { get; set; }
+
+        [StringLength(100)]
+        public string pic { get; set; }
+
+        [StringLength(100)]
+        public string note { get; set; }
+
+        [StringLength(500)]
+        public string moTa { get; set; }
+
+        [StringLength(100)]
+        public string near { get; set; }
+
+        public int? countHomeStay { get; set; }
+
+        public int? countTaxi { get; set; }
+
+        public int? countWL { get; set; }
+
+        public int? countTour { get; set; }
+
+        public double? Cost { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Blog> Blogs { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<DetailCart> DetailCarts { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<HomeStay> HomeStays { get; set; }
+
+        public virtual Province Province { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Taxi> Taxis { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Trip> Trips { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Trip> Trips1 { get; set; }
+    }
+}
